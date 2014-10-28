92a5fc06c06665be976c9bb5a98cb6c28a195296
picksomething <zzucaobin@163.com>
2014-10-28 17:14:06 +0800

    add comments and modify readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Hello,GitHub!
+this repository is for c/c++ codes
--- test.cpp
+++ test.cpp
@@ -2,6 +2,10 @@
 #include<cstdlib>
 using namespace std;
 int main(){
+    /*int *pa;
+     pa = &i;
+     *pa = 2*/
+    //上面注释中的写法是不对的，指针变量必须要初始化之后才能进行其它操作
     int i = 3;
     int *str = NULL;
     str = &i;
